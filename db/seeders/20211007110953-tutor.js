8f3e5da048b3cf5964890e358fc97755465d0443
fabiola <fabiolaelena98@gmail.com>
2021-10-09 07:36:02 -0300

    Creando y buscando errores para que funcione

---
+++ db/seeders/20211007110953-tutor.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Usuarios', [
+      {
+        Apellido: 'Sanchez',
+        Nombre: 'Laura',
+        DNI: '25.567.890',
+        Telefono: '11-34567890',
+        Direccion: 'Belgrano 789',
+        Localidad: 'Moron',
+        Correo: 'slaura@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        Apellido: 'Perez',
+        Nombre: 'Andres',
+        DNI: '22.456.734',
+        Telefono: '11-34024567',
+        Direccion: 'Bolivar 340',
+        Localidad: 'Lanus',
+        Correo: 'andresp@gmail.com',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tutores', null, {});
+  },
+};
