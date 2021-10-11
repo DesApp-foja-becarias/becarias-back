79d3ff372a70fb45a6fab22a7cab190d24b98803
Roman <r_af_95@hotmail.com>
2021-10-11 17:18:26 -0300

    cuenta

---
+++ db/migrations/20211011191150-create-cuenta.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Cuenta', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      banco: {
+        type: Sequelize.STRING,
+      },
+      titularDeCuenta: {
+        type: Sequelize.STRING,
+      },
+      numeroDeCuenta: {
+        type: Sequelize.STRING,
+      },
+      cbu: {
+        type: Sequelize.INTEGER,
+      },
+      tipoDeCuenta: {
+        type: Sequelize.STRING,
+      },
+      sucursal: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Cuenta');
+  },
+};
