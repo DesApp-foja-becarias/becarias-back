9407c206fcd6584dfea7e759a8b15eeb8f28186c
RosaAlfonso1993 <rosa.alfonso1993@gmail.com>
2021-10-12 13:08:03 -0300

    entidad becaria y datos de prueba creados y funcionando

---
+++ db/migrations/20211009203951-create-becaria.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Becaria', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      nombre: {
+        type: Sequelize.STRING,
+      },
+      apellido: {
+        type: Sequelize.STRING,
+      },
+      dni: {
+        type: Sequelize.STRING,
+      },
+      fechaNacimiento: {
+        type: Sequelize.DATE,
+      },
+      telefono: {
+        type: Sequelize.INTEGER,
+      },
+      direccion: {
+        type: Sequelize.STRING,
+      },
+      localidad: {
+        type: Sequelize.STRING,
+      },
+      correo: {
+        type: Sequelize.STRING,
+      },
+      cuit: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Becaria');
+  },
+};
