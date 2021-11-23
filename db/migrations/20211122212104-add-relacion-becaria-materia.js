9bf173476810f338a2a769a1d5a8eb75683a6b99
RosaAlfonso1993 <rosa.alfonso1993@gmail.com>
2021-11-23 19:20:17 -0300

    actualizando con nuevo error

--- db/migrations/20211122212104-add-relacion-becaria-materia.js
+++ db/migrations/20211122212104-add-relacion-becaria-materia.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Becarias-Materias', {
+    await queryInterface.createTable('BecariasMaterias', {
       id: {
         allowNull: false,
         autoIncrement: true,
