78d3aef3efe28c951428617d30533eaf007ecfff
Rosa Alfonso <54155585+RosaAlfonso1993@users.noreply.github.com>
2022-04-28 19:36:24 -0300

    Modificando atributos de actividades (#22)
    * se modificaron los atributos de actividad y se creo la funcionalidad de enviar mail
    
    * cambios en la relacion becaria-actividad y otros pequeños cambios en modelos

--- db/migrations/20211015193547-create-actividad.js
+++ db/migrations/20211015193547-create-actividad.js
@@ -11,6 +11,18 @@ module.exports = {
       name: {
         type: Sequelize.STRING,
       },
+      startDate: {
+        type: Sequelize.DATE,
+      },
+      endDate: {
+        type: Sequelize.DATE,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      validity: {
+        type: Sequelize.BOOLEAN,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
