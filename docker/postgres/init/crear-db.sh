010405109fa513a47fe95da175ea0532c646ecc3
alberto leandro sanchez <albertoleandrosanchez@hotmail.com>
2022-06-06 20:05:38 -0300

    clean routes and models

--- docker/postgres/init/crear-db.sh
+++ docker/postgres/init/crear-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --dbname "$POSTGRES_DB" --username "$POSTGRES_USER" <<-EOSQL
   CREATE USER unahur_desapp WITH PASSWORD 'desarrollo';
 
   CREATE DATABASE unahur_desapp_dev;
